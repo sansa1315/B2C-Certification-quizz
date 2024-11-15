902d47a80f43f02b88e13eaf9c18f7961703b919
sansa1315 <b.espinal1315@gmail.com>
2024-11-15 05:41:56 -0400

    improve code

--- src/components/RadioButton.vue
+++ src/components/RadioButton.vue
@@ -1,38 +1,84 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, computed } from 'vue';
     import {storeToRefs} from 'pinia'
     import {useDataStore} from "@/store/store"
 
     let vocals = ["A","B","C","D"];
     const dataStore = useDataStore();
     const {data, index, renderedQuestionNumber} = storeToRefs(dataStore);
-    let selectedOption = ref(null);
+    let selectedOption = ref(null);      
 
-    function handleSelectedAnswer() {
-        data.value.questions[renderedQuestionNumber.value[index.value - 1]].isAnswered = true;
-        data.value.questions[renderedQuestionNumber.value[index.value - 1]].answers[selectedOption.value].isSelected = true;
-    }
+    const currentQuestion = computed(() => {        
+        return data.value.questions[renderedQuestionNumber.value[index.value - 1]]
+
+    });
+
+    const toggleSelection = (answerIndex) => {
+        const answer = currentQuestion.value.answers[answerIndex];
+        answer.isSelected = !answer.isSelected; 
+    };
+
+    const selectSingleOption = (answerIndex) => {
+        currentQuestion.value.answers.forEach((answer, i) => {
+            answer.isSelected = i === answerIndex; 
+        });
+    };
+
+    const handleSelectedAnswer = () => {
+        currentQuestion.value.isAnswered = true;
+    };
 
 </script>
 
 <template>
-    <div class="d-flex" v-for="answers, i in data.questions[renderedQuestionNumber[index - 1]]?.answers" :key="i">
-        <span>{{vocals[i]}}</span>
-        <div class="form-check ml-1" :class="{'green': answers.isSelected && answers.correct || !answers.isSelected && answers.correct && data.questions[renderedQuestionNumber[index - 1]].isAnswered, 'red': answers.isSelected && !answers.correct}" >
-            <div v-if="answers.isMultiple">
-                <input class="form-check-input" :value="i"  type="checkbox" :checked="answers.isSelected" v-model="selectedOption" :disabled="data.questions[renderedQuestionNumber[index - 1]]?.isAnswered">
+    <div 
+        class="d-flex" 
+        v-for="(answer, i) in currentQuestion.answers" 
+        :key="i"
+    >
+        <span>{{ vocals[i] }}</span>
+        <div 
+            class="form-check ml-1" 
+            :class="{
+                'green': currentQuestion.isAnswered && answer.correct,
+                'red': currentQuestion.isAnswered && answer.isSelected && !answer.correct
+            }"
+        >
+            <div v-if="currentQuestion.isMultiple">
+                <input 
+                    class="form-check-input" 
+                    type="checkbox" 
+                    :checked="answer.isSelected" 
+                    @change="toggleSelection(i)" 
+                    :disabled="currentQuestion.isAnswered"
+                >
             </div>
             <div v-else>
-                <input class="form-check-input" :value="i"  type="radio" :checked="answers.isSelected" v-model="selectedOption" :disabled="data.questions[renderedQuestionNumber[index - 1]]?.isAnswered">
+                <input 
+                    class="form-check-input" 
+                    type="radio" 
+                    :checked="answer.isSelected" 
+                    @change="selectSingleOption(i)" 
+                    :disabled="currentQuestion.isAnswered"
+                >
             </div>
             <label class="form-check-label">
-                {{answers.option}}
+                {{ answer.option }}
             </label>
         </div>
     </div>
     <br>
-    <button class="btn btn-warning mx-2" :class="{'d-none': data.questions[renderedQuestionNumber[index - 1]]?.isAnswered}" @click="handleSelectedAnswer">Validar</button>
+    <button 
+        class="btn btn-warning mx-2" 
+        :class="{'d-none': currentQuestion.isAnswered}" 
+        @click="handleSelectedAnswer"
+    >
+        Validar
+    </button>
 </template>
+
+
+
 
 <style scoped>
     .green{
